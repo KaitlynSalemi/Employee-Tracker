23e77881903a406ac135bb55af9c7873bb362574
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-01-24 22:26:27 -0600

    added inquirer path

--- schema.sql
+++ schema.sql
@@ -4,21 +4,30 @@ CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NULL
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NULL, 
     salary DECIMAL NULL,
     department_id INT NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT NULL, 
     manager_id INT NULL
 );
+
+INSERT INTO department (name)
+VALUES ("production"), ("development"), ("human resource"), ("marketing"), ("purchasing");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 200000, 1), ("COO", 30000, 2), ("CFO", 60000, 3), ("CIO", 750000, 4), ("CTO", 600000, 5);
+
+INSERT INTO role (first_name, last_name, role_id, manager_id)
+VALUES ("James", "Smith", 1, 6), ("Mary", "Johnson", 2, 7), ("Patricia", "Williamson", 3, 7), ("John", "Jones", 4, 6), ("Jennifer", "Miller", 5, 8);
--- server.js
+++ server.js
@@ -20,5 +20,139 @@ connection.connect(function (err) {
 })
 
 function whatDo(){
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+              "Add a Department, Role, or Employee?",
+              "View the data for Departments, Roles, or Employees?",
+              "Update Employee Roles"
+            ]
+        })
+        .then(function(answer) {
+            // console.log(answer);
+            
+            switch (answer.action) {
+                case "Add a Department, Role, or Employee?":
+                addWhat();
+                break;
+        
+                case "View the data for Departments, Roles, or Employees?":
+                viewWhat();
+                break;
+        
+                case "Update Employee Roles":
+                updateRoles();
+                break;
+            }
+        });
+}
 
+function addWhat(){
+    inquirer
+        .prompt([
+            {
+                name: "add",
+                type: "rawlist",
+                message: "What would you like to add?",
+                choices: [
+                    "Department",
+                    "Role",
+                    "Employee",
+                ]
+            },
+            {
+                when: (answers) => answers.add === "Department",
+                name: "department",
+                type: "input",
+                message: "Department Name: "
+            },
+            {
+                when: (answers) => answers.add === "Role",
+                name: "roleTitle",
+                type: "input",
+                message: "Role Title: "
+            },
+            {
+                when: (answers) => answers.add === "Role",
+                name: "salary",
+                type: "input",
+                message: "Salary for that role: "
+            },
+            {
+                when: (answers) => answers.add === "Role",
+                name: "departmentID",
+                type: "input",
+                message: "Department id: "
+            },
+            {
+                when: (answers) => answers.add === "Employee",
+                name: "firstname",
+                type: "input",
+                message: "First Name: "
+            },
+            {
+                when: (answers) => answers.add === "Employee",
+                name: "lastname",
+                type: "input",
+                message: "Last Name: "
+            },
+            {
+                when: (answers) => answers.add === "Employee",
+                name: "roleID",
+                type: "input",
+                message: "Role ID: "
+            },
+            {
+                when: (answers) => answers.add === "Employee",
+                name: "managerID",
+                type: "input",
+                message: "Manager ID: "
+            }
+        ])
+        .then(function(add){
+            console.log(add);
+              
+        })
+}
+
+function viewWhat(){
+    inquirer
+        .prompt({
+            name: "view",
+            type: "rawlist",
+            message: "What data would you like to view?",
+            choices: [
+              "Departments",
+              "Roles",
+              "Employees"
+            ]
+        })
+        .then(function(answer){
+            console.log(answer);
+            
+        })
+}
+
+function updateRoles(){
+    inquirer
+        .prompt ([
+            {
+                name: "updateRole",
+                type: "rawlist",
+                message: "What is the id of the role you would like to update?",
+                choices: ["1", "2", "3", "4", "5"]
+            },
+            {
+                name: "newRole",
+                type: "input",
+                message: "What is the Role Title you would like to change is to?"
+            }
+        ])
+        .then(function(answer){
+            console.log(answer);
+            
+        })
 }
